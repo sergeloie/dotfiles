1ebe97a4c76575562c69ae4028c33333091761b2
Sergey Gusev <serge.loie@yandex.ru>
2024-02-23 20:48:14 +0500

    Create build.gradle.kts

---
+++ toml/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    jacoco
+    id(libs.plugins.patrikerdesUseLatestVersionsPlugin.get().pluginId) version libs.plugins.patrikerdesUseLatestVersionsPlugin.get().version.displayName
+    id(libs.plugins.freefairLombokPLugin.get().pluginId) version libs.plugins.freefairLombokPLugin.get().version.displayName
+    id(libs.plugins.benManesVersionsPlugin.get().pluginId) version libs.plugins.benManesVersionsPlugin.get().version.displayName
+    id(libs.plugins.johnrengelmanShadowPlugin.get().pluginId) version libs.plugins.johnrengelmanShadowPlugin.get().version.displayName
+    id(libs.plugins.littlerobotsVersionCatalogUpdatePlugin.get().pluginId) version libs.plugins.littlerobotsVersionCatalogUpdatePlugin.get().version.displayName
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform(libs.junitBom))
+    testImplementation(libs.jupiter)
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    implementation(libs.bundles.jacksonBundle)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
