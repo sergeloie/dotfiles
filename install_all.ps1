8ec009413738f653a63dc81d25f7be1bf74afab5
Sergey Gusev <serge.loie@yandex.ru>
2025-07-31 15:05:57 +0300

    Create install_all.ps1

---
+++ install_all.ps1
@@ -0,0 +1,33 @@
+choco install vcredist140 -y
+choco install dotnetfx -y
+choco install directx -y
+choco install 7zip.install -y
+choco install firefox -y
+choco install tor-browser -y
+choco install palemoon -y
+choco install thorium -y
+choco install git.install -y
+choco install gradle --version 8.14.2 -y
+choco install libericajdkfull --version 21.0.5.11 -y
+choco install intellijidea-ultimate --version 2025.1.4.1 -y
+choco install jmeter -y
+choco install bruno -y
+choco install dbeaver -y
+choco install visualvm -y
+choco install far -y
+choco install keepassxc -y
+choco install notepadplusplus.install -y
+choco install vmwareworkstation --version 17.6.3.24583834 -y
+choco install fsviewer -y
+choco install foobar2000 -y
+choco install potplayer -y
+choco install wsl2 -y
+choco install docker-desktop -y
+choco install anaconda3 -y
+choco install sysinternals -y
+choco install koodo-reader -y
+choco install treesizefree -y
+choco install shutup10 -y
+choco install v2rayn -y
+choco install ventoy -y
+choco install driverbooster -y
